348b9f2d2aa0444733c2a813eb4513bba29786ec
surverohan <rohansurve26@gmail.com>
2020-02-10 06:42:56 -0600

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 /* Created by Rohan Surve on 02/08/2020. Application  bootstraping with listener configuration */
 
 func main() {
-	
 	router := AppRouter()
 	log.Fatal(http.ListenAndServe(":8080", router))
 
